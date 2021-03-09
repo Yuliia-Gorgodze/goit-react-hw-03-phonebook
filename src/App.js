1e14eb7037816f8ffacbc33bff1eede0dd883f3f
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-03-09 10:34:18 +0200

    comit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import shortid from 'shortid';
 //components
-import AddNameContact from './components/Form'
-import ContactList from './components/ContactList'
-import Filter from './components/Filter'
-import styles from './components/styles.module.css'
+import AddNameContact from './components/Form';
+import ContactList from './components/ContactList';
+import Filter from './components/Filter';
+import styles from './components/styles.module.css';
 
 class App extends Component {
-    state = {
-        contacts: [ 
-        ],
-        filter: ''
-      }
-  componentDidUpdate(prevProps, prevState){  
-  if(this.state.contacts !== prevState.contacts){
-     localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
-  }
-  componentDidMount(){
-      const contacts = localStorage.getItem('contacts');
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
       const parseContacts = JSON.parse(contacts);
-      this.setState({contacts: parseContacts})
+      this.setState({ contacts: parseContacts });
+    } else {
+      return;
+    }
   }
-checkContact = name =>{
-    return this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
-}
-   addContact = ({name, number}) =>{
-       const contact = {
-           id: shortid.generate(),
-           name, 
-           number
-       }
-       if(this.checkContact(name)){
-           alert('Такой контакт уже есть !!!!')
-           return
-       }else{
-        this.setState(({contacts}) => ({
-            contacts: [contact, ...contacts],
-        }))
-       }
-   console.log(this.state.contacts)
-   }
-   deleteCantact = (id) => {
-this.setState(prevState => ({
-    contacts: prevState.contacts.filter(contact => contact.id !== id),
-}))
-   }
-   inputChange = event => {
-    const {name, value} = event.currentTarget;
-    this.setState({[name]: value})
-}
-   getVisibleContact = () => {
-   const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
-   }
-render(){
-  return(
-    
-    <div>
+  checkContact = name => {
+    return this.state.contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase(),
+    );
+  };
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    if (this.checkContact(name)) {
+      alert('Такой контакт уже есть !!!!');
+      return;
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    }
+    console.log(this.state.contacts);
+  };
+  deleteCantact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+  inputChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+  getVisibleContact = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+  render() {
+    return (
+      <div>
         <h1>Телефонная книга</h1>
-        <AddNameContact formSabmit={this.addContact}/>
+        <AddNameContact formSabmit={this.addContact} />
         <h2>Мои контакты: </h2>
-        <Filter inputChange={this.inputChange} state={this.state.filter}/>
-        <ContactList contacts={this.getVisibleContact()} deleteCantact={this.deleteCantact}/>
-    </div>
-  )
-}
+        <Filter inputChange={this.inputChange} state={this.state.filter} />
+        <ContactList
+          contacts={this.getVisibleContact()}
+          deleteCantact={this.deleteCantact}
+        />
+      </div>
+    );
+  }
 }
 export default App;
